c2ff3fac26e6b92a708dbcb4885fc201ff8e28fb
Viper <contact@vipertech.ch>
2021-06-14 11:50:17 +0200

    SW update again and again f*

--- service-worker.js
+++ service-worker.js
@@ -68,61 +68,10 @@ self.addEventListener('install', function(evt) {
   }));
 });
 
-// On fetch, use cache but update the entry with the latest contents
-// from the server.
-self.addEventListener('fetch', function(evt) {
-  console.log('The service worker is serving the asset.');
-  // You can use `respondWith()` to answer ASAP...
-  evt.respondWith(fromCache(evt.request));
-  // ...and `waitUntil()` to prevent the worker to be killed until
-  // the cache is updated.
-  evt.waitUntil(
-      update(evt.request)
-          // Finally, send a message to the client to inform it about the
-          // resource is up to date.
-          .then(refresh)
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
   );
 });
-
-// Open the cache where the assets were stored and search for the requested
-// resource. Notice that in case of no matching, the promise still resolves
-// but it does with `undefined` as value.
-function fromCache(request) {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.match(request);
-  });
-}
-
-
-// Update consists in opening the cache, performing a network request and
-// storing the new response data.
-function update(request) {
-  return caches.open(CACHE).then(function (cache) {
-    return fetch(request).then(function (response) {
-      return cache.put(request, response.clone()).then(function () {
-        return response;
-      });
-    });
-  });
-}
-
-// Sends a message to the clients.
-function refresh(response) {
-  return self.clients.matchAll().then(function (clients) {
-    clients.forEach(function (client) {
-      // Encode which resource has been updated. By including the
-      // [ETag](https://en.wikipedia.org/wiki/HTTP_ETag) the client can
-      // check if the content has changed.
-      var message = {
-        type: 'refresh',
-        url: response.url,
-        // Notice not all servers return the ETag header. If this is not
-        // provided you should use other cache headers or rely on your own
-        // means to check if the content has changed.
-        eTag: response.headers.get('ETag')
-      };
-      // Tell the client about the update.
-      client.postMessage(JSON.stringify(message));
-    });
-  });
-}
