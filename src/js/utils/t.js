42160a2eb58b921f43bccd19489a6b8f5d9a207d
Viper <contact@vipertech.ch>
2021-07-14 22:59:04 +0200

    SVG media in wiki added on all of it's sub-pages

--- src/js/utils/t.js
+++ src/js/utils/t.js
@@ -1,18 +1,37 @@
-import { clean_json_text } from "../utils/json";
 import svg64 from "svg64";
 
+import svg from "../locales/svg";
 import en from "../locales/en";
 import fr from "../locales/fr";
 
-const T = { en, fr };
+const T = { svg, en, fr };
 
 function t(path = "", variables = {}, parameters = {}) {
 
+    const PARAM_PROPS_NAME = ["faw", "fluc", "flc", "fllc", "tuc", "tlc", "aed"];
+    const LANG = document.documentElement.lang;
+    const LANG_DIR = T[LANG] || T["en"];
+
+    // Detect if variables has been used as parameters for shorter syntax
+    let variables_is_in_fact_parameters = false;
+    Object.entries(variables).forEach(([key, value] ) => {
+
+        variables_is_in_fact_parameters = PARAM_PROPS_NAME.includes(key.toLowerCase());
+    });
+
+    if(variables_is_in_fact_parameters) {
+
+        parameters = variables;
+        variables = {};
+    }
+
+    // Split path every dot
     const paths_array = path.split(".");
 
-    // Goes to the last dir specified in the path
-    let dir = T[document.documentElement.lang] || T["en"];
+    // Goes to the first dir, (it is the language)
+    let dir = LANG_DIR;
 
+    // Goes to all other dir except pre-first (lang) and last (value :Object or :String)
     for(let i = 0; i < paths_array.length-1; i++) {
 
         dir = dir[paths_array[i]];
@@ -21,7 +40,8 @@ function t(path = "", variables = {}, parameters = {}) {
     // Get the object in the last dir
     let end_dir_name = "";
 
-    if(parameters.faw || parameters.FAW || variables.faw || variables.FAW) { // Format All Words
+    // Needs to remove '"', "'", ",", and so on so it can look for the right sentence... "My computer's mouse" -> "my computers mouse"
+    if(parameters.faw || parameters.FAW) { // Format All Words
 
         end_dir_name = paths_array[paths_array.length-1]
             .replaceAll("'", "")
@@ -29,116 +49,156 @@ function t(path = "", variables = {}, parameters = {}) {
             .replaceAll(",", "")
             .toLowerCase();
 
-        variables.faw = null;
-        variables.FAW = null;
-
     }else {
 
         end_dir_name = paths_array[paths_array.length-1];
     }
 
+    // It select the array property by its name so it is important to format it eventually if specified by FAW parameters...
     const end_path_value = dir[end_dir_name];
 
+    // Stringify so it supports objects
+    let value_with_variables = JSON.stringify(end_path_value);
+
     // Replace variables with real value(s) in the translate object value chosen
-    let value_with_variables = end_path_value;
-    value_with_variables = clean_json_text(JSON.stringify(value_with_variables));
+    Object.entries(variables).forEach(variable => {
 
-    Object.entries(variables).forEach(entry => {
+        const [
+            variable_name_to_replace, // dog
+            variable_value_to_replace // "dog" or {dog: 2}
+        ] = variable;
 
-        const [key, value] = entry;
+        if(variable_name_to_replace.length || variable_value_to_replace.toString()){
 
-        if(key.length || value.toString()){
+            const is_value_a_plural = typeof variable_value_to_replace === "object" && variable_value_to_replace !== null && variable_value_to_replace !== 0;
 
-            if(!["fluc", "flc", "fllc", "tuc", "tlc", "aed"].includes(key.toLowerCase())) {
+            if(is_value_a_plural) { // {dog: 2}
 
-                let variable_name_to_replace = key; // dog
-                let variable_value_to_replace = value; // "dog" or {dog: 2}
+                const [
+                    variable_value_to_replace_props_pair, // dog: 2 in array mode
+                    variable_number_to_replace_props_pair // _n: {few: 20, plenty: 100} in array mode
+                ] = Object.entries(variable_value_to_replace); // {dog: 2, _n: {few: 20, plenty: 100} } in array mode
 
-                const is_value_a_plural = typeof variable_value_to_replace === "object" && variable_value_to_replace !== null;
+                const [
+                    start_plural_key, // dog
+                    plural_number_value // 2
+                ] = variable_value_to_replace_props_pair;
 
-                if(is_value_a_plural) { // {dog: 2}
+                if(start_plural_key === variable_name_to_replace) {
 
-                    const start_plural_key = Object.entries(variable_value_to_replace)[0][0]; // dog
+                    let few_how_much = null;
+                    let plenty_how_much = null;
 
-                    if(start_plural_key === variable_name_to_replace) {
+                    const [
+                        variable_value_to_replace_props_pair_name_id, // _n
+                        variable_number_to_replace_props_pair_value_pair // {few: 20, plenty: 100}
+                    ] = Object.entries(variable_value_to_replace_props_pair);
 
-                        let few_how_much = null;
-                        let plenty_how_much = null;
+                    if (variable_value_to_replace_props_pair_name_id === "_n"){
 
-                        if (Object.entries(variable_value_to_replace)[1][0] === "_n"){  // {dog: 2, _n: {few: 20, plenty: 100}}
-
-                            few_how_much = Object.entries(variable_value_to_replace)[1][1]["few"] || null;
-                            plenty_how_much = Object.entries(variable_value_to_replace)[1][1]["plenty"] || null;
-                        }
-
-                        const plural_number_value = Object.entries(variable_value_to_replace)[0][1]; // 2
-                        const need_to_find_string = "%s {{" + start_plural_key + "}}";
-
-                        let numbered_plural_var_text = plural_number_value <= 1 ? // Either one or many
-                            T[document.documentElement.lang]["_plurals"][start_plural_key]["one"].toString():
-                            T[document.documentElement.lang]["_plurals"][start_plural_key]["many"].toString();
-
-
-                        if(few_how_much && plural_number_value > 1 && plural_number_value < few_how_much){  // From one up to few
-
-                            numbered_plural_var_text =  T[document.documentElement.lang]["_plurals"][start_plural_key]["few"].toString();
-                        }
-
-                        if(plenty_how_much && plural_number_value >= plenty_how_much) { // From plenty up to infinity
-
-                            numbered_plural_var_text =  T[document.documentElement.lang]["_plurals"][start_plural_key]["plenty"].toString();
-                        }
-
-                        const need_to_replace_string = plural_number_value.toString() + " " + numbered_plural_var_text;
-
-                        if(value_with_variables.includes(need_to_find_string)) {
-
-                            value_with_variables = value_with_variables.replaceAll(need_to_find_string, need_to_replace_string);
-                        }
+                        few_how_much = variable_number_to_replace_props_pair_value_pair["few"] || null;
+                        plenty_how_much = variable_number_to_replace_props_pair_value_pair["plenty"] || null;
                     }
 
-                }else { // "dog"
+                    const need_to_find_string = "%n {{{_plurals." + start_plural_key + "}}}";
 
-                    const need_to_find_string = "{{" + variable_name_to_replace + "}}";
-                    const need_to_replace_string = variable_value_to_replace === null ? "": variable_value_to_replace.toString();
+                    let numbered_plural_var_text = plural_number_value <= 1 ? // Either one or many
+                        LANG_DIR["_plurals"][start_plural_key]["one"]:
+                        LANG_DIR["_plurals"][start_plural_key]["many"];
 
-                    if(value_with_variables.includes(need_to_find_string)) {
 
-                        value_with_variables = value_with_variables.replaceAll(need_to_find_string, need_to_replace_string);
-                    }
+                    // From one up to few
+                    numbered_plural_var_text = (few_how_much && plural_number_value > 1 && plural_number_value < few_how_much) ?
+                        LANG_DIR["_plurals"][start_plural_key]["few"]: numbered_plural_var_text;
+
+
+                    // From plenty up to infinity
+                    numbered_plural_var_text = (plenty_how_much && plural_number_value >= plenty_how_much) ?
+                        LANG_DIR["_plurals"][start_plural_key]["plenty"]: numbered_plural_var_text;
+
+
+                    const need_to_replace_string = plural_number_value + " " + numbered_plural_var_text;
+
+                    value_with_variables = value_with_variables.replace(need_to_find_string, need_to_replace_string);
                 }
+            }else { // "dog"
+
+                const need_to_find_string = "{{" + variable_name_to_replace + "}}";
+                const need_to_replace_string = variable_value_to_replace === null ? "": variable_value_to_replace.toString();
+
+                value_with_variables = value_with_variables.replaceAll(need_to_find_string, need_to_replace_string);
 
             }
         }
 
     });
 
-    let svg_image_regex = /\{\{\{\!\[(.*?)?(\]\(data\:image\/svg\+xml\;utf8\,(.*?)\))(\}\}\})/gm;
+    const svg_image_regex = /\{\{\{_svg\.(.*?)\}\}\}/gm;
 
     let svg_image_regex_match;
     while((svg_image_regex_match = svg_image_regex.exec(value_with_variables)) !== null) {
 
-        if(typeof svg_image_regex_match[3] !== "undefined") {
+        const svg_image_key = svg_image_regex_match[1];
 
-            let svg_image_string = svg_image_regex_match[3]
-                .replaceAll(/\\\"/g, `"`)
-                .replaceAll(/\\\'/g, `'`);
+        if(typeof svg_image_key !== "undefined") {
 
-            value_with_variables = value_with_variables
-                .replace(
-                    svg_image_regex_match[0],
-                    svg_image_regex_match[0]
-                        .replace("data:image/svg+xml;utf8,", "")
-                        .replace(svg_image_regex_match[3], svg64(svg_image_string))
-                        .replace("{{{", " ")
-                        .replace("}}}", " ")
-                );
+            let svg_image = "";
+
+            if(typeof T["svg"][svg_image_key] !== "undefined") {
+
+                svg_image = T["svg"][svg_image_key];
+
+                const svg_image_params = LANG_DIR["_svg"][svg_image_key] || {};
+
+                Object.entries(svg_image_params).forEach(svg_image_params_entry => {
+
+                    const [
+                        svg_image_params_variable_name_to_replace,
+                        svg_image_params_variable_value_to_replace
+                    ] = svg_image_params_entry;
+
+                    if(svg_image_params_variable_name_to_replace.length || svg_image_params_variable_value_to_replace.toString()){
+
+                        svg_image = svg_image.replaceAll("{{"+svg_image_params_variable_name_to_replace+"}}", svg_image_params_variable_value_to_replace);
+                    }
+
+                });
+
+                let svg_image_for_replace_regex = /\!\[(.*?)?(\]\(data\:image\/svg\+xml\;utf8\,(.+)\))/gm;
+
+                let svg_image_for_replace_regex_match;
+                while((svg_image_for_replace_regex_match = svg_image_for_replace_regex.exec(svg_image)) !== null) {
+
+                    if(typeof svg_image_for_replace_regex_match[3] !== "undefined") {
+
+                        let svg_image_string = svg_image_for_replace_regex_match[3]
+                            .replaceAll(/\\\"/g, `"`)
+                            .replaceAll(/\\\'/g, `'`);
+
+                        svg_image = svg_image
+                            .replace(
+                                svg_image_for_replace_regex_match[0],
+                                svg_image_for_replace_regex_match[0]
+                                    .replace("data:image/svg+xml;utf8,", "")
+                                    .replace(svg_image_for_replace_regex_match[3], svg64(svg_image_string))
+                            );
+
+                    }
+                }
+
+            }
+
+            const need_to_find_string = "{{{_svg." + svg_image_key + "}}}";
+            const need_to_replace_string = svg_image;
+
+            value_with_variables = value_with_variables.replaceAll(need_to_find_string, need_to_replace_string);
 
         }
     }
 
     value_with_variables = JSON.parse(value_with_variables);
     parameters = Object.assign(variables, parameters);
+
     if(parameters.fluc || parameters.FLUC || parameters.flc || parameters.FLC) { // First Letter Upper Case
 
         value_with_variables = value_with_variables.charAt(0).toUpperCase() + value_with_variables.slice(1);
@@ -170,4 +230,3 @@ function t(path = "", variables = {}, parameters = {}) {
 module.exports = {
     t
 };
-
