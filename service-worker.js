e76997d3e34f4148e132f8c997274c958431b5f7
Viper <contact@vipertech.ch>
2021-07-14 04:40:52 +0200

    Responsive Wiki SVG fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var CACHE = "network-or-cache-v12.20";
+var CACHE = "network-or-cache-v12.21";
 
 // On install, cache some resource.
 self.addEventListener("install", function(evt) {
@@ -10,7 +10,7 @@ self.addEventListener("install", function(evt) {
     cache.addAll([
       "/",
       "/404.html",
-      "/client.min.js?v=12.20",
+      "/client.min.js?v=12.21",
       "src/fonts/Cantarell-Regular.ttf",
       "src/fonts/OpenSans-Regular.ttf",
       "src/fonts/Saira-Regular.ttf",
