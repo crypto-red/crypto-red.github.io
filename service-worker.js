956f27a5297a4d0d193a93e4bd7b7561dd47099b
Viper <contact@vipertech.ch>
2021-11-08 03:29:39 +0100

    Bottom text situationist of development in pixel art editor

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var CACHE = "network-or-cache-v22.20";
+var CACHE = "network-or-cache-v22.21";
 
 // On install, cache some resource.
 self.addEventListener("install", function(evt) {
@@ -10,7 +10,7 @@ self.addEventListener("install", function(evt) {
     cache.addAll([
       "/",
       "/404.html",
-      "/client.min.js?v=22.20",
+      "/client.min.js?v=22.21",
       "/src/fonts/NotoSans-Regular.ttf",
       "/src/fonts/NotoSansMono-Regular.ttf",
       "/src/fonts/Saira-Regular.ttf",
