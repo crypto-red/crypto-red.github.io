248752165f990f90bedca7247662c107cb12135e
Viper <contact@vipertech.ch>
2021-11-25 00:28:54 +0100

    Background home change

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var CACHE = "network-or-cache-v30.9";
+var CACHE = "network-or-cache-v30.10";
 
 // On install, cache some resource.
 self.addEventListener("install", function(evt) {
@@ -10,7 +10,7 @@ self.addEventListener("install", function(evt) {
     cache.addAll([
       "/",
       "/404.html",
-      "/client.min.js?v=30.9",
+      "/client.min.js?v=30.10",
       "/src/fonts/NotoSans-Regular.ttf",
       "/src/fonts/NotoSansMono-Regular.ttf",
       "/src/fonts/Saira-Regular.ttf",
