a5c70eb46189bac8cd4b638f16e0ea6c2af3d520
Viper <contact@vipertech.ch>
2021-11-26 02:34:13 +0100

    Account profile hive background

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var CACHE = "network-or-cache-v30.14";
+var CACHE = "network-or-cache-v30.15";
 
 // On install, cache some resource.
 self.addEventListener("install", function(evt) {
@@ -10,7 +10,7 @@ self.addEventListener("install", function(evt) {
     cache.addAll([
       "/",
       "/404.html",
-      "/client.min.js?v=30.14",
+      "/client.min.js?v=30.15",
       "/src/fonts/NotoSans-Regular.ttf",
       "/src/fonts/SpecialElite-Regular.ttf",
       "/src/fonts/NotoSansMono-Regular.ttf",
