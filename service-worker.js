28fdee2bf107cc1bd82db733fcdde2dc3ad765a7
Viper <contact@vipertech.ch>
2021-12-28 01:48:21 +0100

    SW //

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-var REQUIRED_CACHE = "network-or-cache-v9-required";
-var USEFUL_CACHE = "network-or-cache-v9-useful";
-var STATIC_CACHE = "network-or-cache-v9-static";
+var REQUIRED_CACHE = "network-or-cache-v10-required";
+var USEFUL_CACHE = "network-or-cache-v10-useful";
+var STATIC_CACHE = "network-or-cache-v10-static";
 
 // On install, cache some resource.
 self.addEventListener("install", function(evt) {
@@ -12,14 +12,14 @@ self.addEventListener("install", function(evt) {
       caches.open(REQUIRED_CACHE).then(function (cache) {
             return cache.addAll([
                 "/",
-                "/index.html",
-                "/404.html",
-                "/client.min.js",
-                "/src/fonts/NotoSans-Regular.woff2",
-                "/src/fonts/SpecialElite-Regular.woff2",
-                "/src/fonts/NotoSansMono-Regular.woff2",
-                "/src/fonts/ShareTechMono-Regular.woff2",
-                "/src/fonts/Saira-Regular.woff2",
+                "//index.html",
+                "//404.html",
+                "//client.min.js",
+                "//src/fonts/NotoSans-Regular.woff2",
+                "//src/fonts/SpecialElite-Regular.woff2",
+                "//src/fonts/NotoSansMono-Regular.woff2",
+                "//src/fonts/ShareTechMono-Regular.woff2",
+                "//src/fonts/Saira-Regular.woff2",
             ]);
       }),
       caches.open(USEFUL_CACHE).then(function (cache) {
@@ -58,7 +58,7 @@ self.addEventListener("fetch", function(event) {
 
     event.respondWith(
         caches.open(USEFUL_CACHE).then(function (cache) {
-          return cache.match("/0.client.min.js").then(function (response) {
+          return cache.match("//0.client.min.js").then(function (response) {
             return (
                 response ||
                 fetch(event.request).then(function (response) { // Fetch, clone, and serve
@@ -74,7 +74,7 @@ self.addEventListener("fetch", function(event) {
 
     event.respondWith(
         caches.open(USEFUL_CACHE).then(function (cache) {
-          return cache.match("/1.client.min.js").then(function (response) {
+          return cache.match("//1.client.min.js").then(function (response) {
             return (
                 response ||
                 fetch(event.request).then(function (response) { // Fetch, clone, and serve
@@ -90,7 +90,7 @@ self.addEventListener("fetch", function(event) {
 
     event.respondWith(
         caches.open(USEFUL_CACHE).then(function (cache) {
-          return cache.match("/2.client.min.js").then(function (response) {
+          return cache.match("//2.client.min.js").then(function (response) {
             return (
                 response ||
                 fetch(event.request).then(function (response) { // Fetch, clone, and serve
@@ -106,7 +106,7 @@ self.addEventListener("fetch", function(event) {
 
     event.respondWith(
         caches.open(USEFUL_CACHE).then(function (cache) {
-          return cache.match("/3.client.min.js").then(function (response) {
+          return cache.match("//3.client.min.js").then(function (response) {
             return (
                 response ||
                 fetch(event.request).then(function (response) { // Fetch, clone, and serve
@@ -122,7 +122,7 @@ self.addEventListener("fetch", function(event) {
 
     event.respondWith(
         caches.open(USEFUL_CACHE).then(function (cache) {
-          return cache.match("/4.client.min.js").then(function (response) {
+          return cache.match("//4.client.min.js").then(function (response) {
             return (
                 response ||
                 fetch(event.request).then(function (response) { // Fetch, clone, and serve
@@ -138,7 +138,7 @@ self.addEventListener("fetch", function(event) {
 
     event.respondWith(
         caches.open(USEFUL_CACHE).then(function (cache) {
-          return cache.match("/5.client.min.js").then(function (response) {
+          return cache.match("//5.client.min.js").then(function (response) {
             return (
                 response ||
                 fetch(event.request).then(function (response) { // Fetch, clone, and serve
@@ -154,7 +154,7 @@ self.addEventListener("fetch", function(event) {
 
     event.respondWith(
         caches.open(USEFUL_CACHE).then(function (cache) {
-          return cache.match("/6.client.min.js").then(function (response) {
+          return cache.match("//6.client.min.js").then(function (response) {
             return (
                 response ||
                 fetch(event.request).then(function (response) { // Fetch, clone, and serve
@@ -170,7 +170,7 @@ self.addEventListener("fetch", function(event) {
 
     event.respondWith(
         caches.open(REQUIRED_CACHE).then(function (cache) {
-          return cache.match("/client.min.js").then(function (response) {
+          return cache.match("//client.min.js").then(function (response) {
             return (
                 response ||
                 fetch(event.request).then(function (response) { // Fetch, clone, and serve
@@ -225,65 +225,65 @@ self.addEventListener("activate", function(event) {
   event.waitUntil(Promise.allSettled([
           caches.open(USEFUL_CACHE).then(function (cache) {
             return cache.addAll([
-              "/0.client.min.js",
-              "/1.client.min.js",
-              "/2.client.min.js",
+              "//0.client.min.js",
+              "//1.client.min.js",
+              "//2.client.min.js",
               //"/3.client.min.js",
-              "/4.client.min.js",
-              "/5.client.min.js",
-              "/6.client.min.js",
-              "/src/images/404-dark-2.svg",
-              "/src/images/analytics.svg",
-              "/src/images/account.svg",
-              "/src/images/account-add.svg",
-              "/src/images/hacker.svg",
-              "/src/images/wallet-green.svg",
-              "/src/images/world_blue.jpg",
-              "/src/images/404-dark.svg",
-              "/src/images/segment.svg",
-              "/src/images/security.svg",
-              "/src/images/share.svg",
-              "/src/images/transfer.svg",
-              "/src/images/wallet.svg",
-              "/src/images/data.svg",
-              "/src/images/dust_overlay.png",
-              "/src/images/logo-transparent.png",
-              "/src/images/personal-finance.svg",
-              "/src/images/statistics.svg",
-              "/src/images/card.svg",
-              "/src/images/favicon.ico",
-              "/src/images/pig-coins.svg",
-              "/src/images/pixelart_card.png",
-              "/src/images/swap.svg",
-              "/src/images/invest.svg",
-              "/src/images/investment-data.svg",
-              "/src/images/open.svg",
-              "/src/images/trade.svg",
+              "//4.client.min.js",
+              "//5.client.min.js",
+              "//6.client.min.js",
+              "//src/images/404-dark-2.svg",
+              "//src/images/analytics.svg",
+              "//src/images/account.svg",
+              "//src/images/account-add.svg",
+              "//src/images/hacker.svg",
+              "//src/images/wallet-green.svg",
+              "//src/images/world_blue.jpg",
+              "//src/images/404-dark.svg",
+              "//src/images/segment.svg",
+              "//src/images/security.svg",
+              "//src/images/share.svg",
+              "//src/images/transfer.svg",
+              "//src/images/wallet.svg",
+              "//src/images/data.svg",
+              "//src/images/dust_overlay.png",
+              "//src/images/logo-transparent.png",
+              "//src/images/personal-finance.svg",
+              "//src/images/statistics.svg",
+              "//src/images/card.svg",
+              "//src/images/favicon.ico",
+              "//src/images/pig-coins.svg",
+              "//src/images/pixelart_card.png",
+              "//src/images/swap.svg",
+              "//src/images/invest.svg",
+              "//src/images/investment-data.svg",
+              "//src/images/open.svg",
+              "//src/images/trade.svg",
             ]);
           }),
           caches.open(STATIC_CACHE).then(function (cache) {
             return cache.addAll([
-              "/src/sounds/sfx/md/alert_error-01.mp3",
-              "/src/sounds/sfx/md/navigation_transition-left.mp3",
-              "/src/sounds/sfx/md/alert_high-intensity.mp3",
-              "/src/sounds/sfx/md/FullHorizonThrow.mp3",
-              "/src/sounds/sfx/md/navigation_transition-right.mp3",
-              "/src/sounds/sfx/md/PrometheusVertical2.mp3",
-              "/src/sounds/sfx/md/hero_decorative-celebration-01.mp3",
-              "/src/sounds/sfx/md/state-change_confirm-down.mp3",
-              "/src/sounds/sfx/md/hero_decorative-celebration-02.mp3",
-              "/src/sounds/sfx/md/state-change_confirm-up.mp3",
-              "/src/sounds/sfx/md/hero_decorative-celebration-03.mp3",
-              "/src/sounds/sfx/md/MazeImpact5.mp3",
-              "/src/sounds/sfx/md/ui_camera-shutter.mp3",
-              "/src/sounds/sfx/md/navigation_backward-selection-minimal.mp3",
-              "/src/sounds/sfx/md/ui_loading.mp3",
-              "/src/sounds/sfx/md/navigation_backward-selection.mp3",
-              "/src/sounds/sfx/md/ui_lock.mp3",
-              "/src/sounds/sfx/md/navigation_forward-selection.mp3",
-              "/src/sounds/sfx/md/ui_tap-variant-01.mp3",
-              "/src/sounds/sfx/md/navigation_selection-complete-celebration.mp3",
-              "/src/sounds/sfx/md/ui_unlock.mp3",
+              "//src/sounds/sfx/md/alert_error-01.mp3",
+              "//src/sounds/sfx/md/navigation_transition-left.mp3",
+              "//src/sounds/sfx/md/alert_high-intensity.mp3",
+              "//src/sounds/sfx/md/FullHorizonThrow.mp3",
+              "//src/sounds/sfx/md/navigation_transition-right.mp3",
+              "//src/sounds/sfx/md/PrometheusVertical2.mp3",
+              "//src/sounds/sfx/md/hero_decorative-celebration-01.mp3",
+              "//src/sounds/sfx/md/state-change_confirm-down.mp3",
+              "//src/sounds/sfx/md/hero_decorative-celebration-02.mp3",
+              "//src/sounds/sfx/md/state-change_confirm-up.mp3",
+              "//src/sounds/sfx/md/hero_decorative-celebration-03.mp3",
+              "//src/sounds/sfx/md/MazeImpact5.mp3",
+              "//src/sounds/sfx/md/ui_camera-shutter.mp3",
+              "//src/sounds/sfx/md/navigation_backward-selection-minimal.mp3",
+              "//src/sounds/sfx/md/ui_loading.mp3",
+              "//src/sounds/sfx/md/navigation_backward-selection.mp3",
+              "//src/sounds/sfx/md/ui_lock.mp3",
+              "//src/sounds/sfx/md/navigation_forward-selection.mp3",
+              "//src/sounds/sfx/md/ui_tap-variant-01.mp3",
+              "//src/sounds/sfx/md/navigation_selection-complete-celebration.mp3",
+              "//src/sounds/sfx/md/ui_unlock.mp3",
             ]);
           }),
           caches.keys().then(keys => Promise.allSettled(
