b3dff5c66d537810cf95a1b54fa8aa208cbb1848
Viper <contact@vipertech.ch>
2022-01-01 04:14:04 +0100

    Updatedcr

--- src/js/components/CanvasPixels.js
+++ src/js/components/CanvasPixels.js
@@ -1,8 +1,8 @@
 /*
 The MIT License (MIT)
 
-Copyright (c) 2021 Vipertech
-Copyright (c) 2021 Crypto red
+Copyright (c) 2021 - 2022 Vipertech
+Copyright (c) 2021 - 2022 Crypto red
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
